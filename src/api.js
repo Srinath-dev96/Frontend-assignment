78883711bfb273c76b1026c6a624cabb4d42eac4
Srinath-dev96 <your-email@example.com>
2025-10-01 14:55:40 +0530

    Use environment variable for API URL

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL; // use environment variable
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const api = axios.create({
   baseURL: `${API_URL}/api`,
@@ -8,8 +8,6 @@ export const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
