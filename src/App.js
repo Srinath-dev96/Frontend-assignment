a34b7f4b8b671bdd8b770bba66af07c284aabc51
Srinath-dev96 <your-email@example.com>
2025-10-01 17:15:41 +0530

    Add default route for root path

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard"; 
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Default route (root) */}
-        <Route path="/" element={<Login />} />
-
-        {/* Other routes */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
